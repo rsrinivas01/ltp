1248dbe60fb704cc60e026bfc04eee5a5b2a3a98
Srinivas. R <rsrinivas01@gmail.com>
2020-08-06 17:06:08 +0000

    fix for issue 280 (rename14 failure)

--- testcases/kernel/syscalls/rename/rename14.c
+++ testcases/kernel/syscalls/rename/rename14.c
@@ -45,125 +45,36 @@
 
 #include "test.h"
 
-#define FAILED 0
-#define PASSED 1
-
-int local_flag = PASSED;
-
 char *TCID = "rename14";
 int TST_TOTAL = 1;
 
-#define RUNTIME	5
-
-int kidpid[2];
-int parent_pid;
-
-int term(void);
-int al(void);
-void dochild1(void);
-void dochild2(void);
-int fd;
-
 int main(int argc, char *argv[])
 {
-	int pid;
-	sigset_t set;
-	struct sigaction act, oact;
+	int fd;
+	int ret = -1;
 
 	tst_parse_opts(argc, argv, NULL, NULL);
 
-	sigemptyset(&set);
-	act.sa_handler = (void (*)())term;
-	act.sa_mask = set;
-	act.sa_flags = 0;
-	if (sigaction(SIGTERM, &act, &oact)) {
-		tst_brkm(TBROK, NULL, "Sigaction(SIGTERM)");
-	}
-
-	sigemptyset(&set);
-	act.sa_handler = (void (*)())al;
-	act.sa_mask = set;
-	act.sa_flags = 0;
-	if (sigaction(SIGALRM, &act, 0)) {
-		tst_brkm(TBROK, NULL, "Sigaction(SIGALRM)");
-	}
-	parent_pid = getpid();
 	tst_tmpdir();
-	dochild1();
-	dochild2();
-#if 0
-	pid = FORK_OR_VFORK();
-	if (pid < 0)
-		tst_brkm(TBROK, NULL, "fork() returned %d", pid);
-	if (pid == 0)
-		dochild1();
+	
+	fd = creat("./rename14", 0666);
+	close(fd);
+	
+	ret = rename("./rename14", "./rename14xyz");
 
-	kidpid[0] = pid;
-	pid = FORK_OR_VFORK();
-	if (pid < 0) {
-		(void)kill(kidpid[0], SIGTERM);
-		(void)unlink("./rename14");
-		tst_brkm(TBROK, NULL, "fork() returned %d", pid);
+	if (ret == 0)
+	{
+		tst_resm(TPASS, "Test Passed");
+	} 
+	else
+	{
+		tst_resm(TFAIL, " Test Failed.. rename returned %d, errno = %d", ret, errno);
 	}
-	if (pid == 0)
-		dochild2();
-
-	kidpid[1] = pid;
-
-	alarm(RUNTIME);
-
-	/* Collect child processes. */
-	/* Wait for timeout */
-	pause();
-
-	kill(kidpid[0], SIGTERM);
-	kill(kidpid[1], SIGTERM);
-
-	waitpid(kidpid[0], NULL, 0);
-	waitpid(kidpid[1], NULL, 0);
-
+	
 	unlink("./rename14");
 	unlink("./rename14xyz");
-	(local_flag == PASSED) ? tst_resm(TPASS, "Test Passed")
-	    : tst_resm(TFAIL, "Test Failed");
-#endif 
+	
 	tst_rmdir();
 	tst_exit();
-}
 
-int term(void)
-{
-	if (parent_pid != getpid())
-		exit(0);
-	if (kidpid[0])
-		return (kill(kidpid[0], SIGTERM));
-	if (kidpid[1])
-		return (kill(kidpid[1], SIGTERM));
-	return 0;
-}
-
-int al(void)
-{
-	if (kidpid[0])
-		return (kill(kidpid[0], SIGTERM));
-	if (kidpid[1])
-		return (kill(kidpid[1], SIGTERM));
-	return 0;
-}
-
-void dochild1(void)
-{
-//	int fd;
-
-	//for (;;) {
-		fd = creat("./rename14", 0666);
-		unlink("./rename14");
-		close(fd);
-	//}
-}
-
-void dochild2(void)
-{
-//	for (;;)
-		rename("./rename14", "./rename14xyz");
 }
